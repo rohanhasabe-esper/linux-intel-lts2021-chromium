9c4b3bc61a3b97e042f2a9d114c623805993ca48
Aiswarya Cyriac <aiswarya.cyriac@intel.com>
2021-09-22 12:28:00 +0530

    Fix for btusb_sco_snd_card not getting probed
    Tracked-On: OAM-99194
    Signed-off-by: Aiswarya Cyriac <aiswarya.cyriac@intel.com>
    Signed-off-by: rajucm <raju.mallikarjun.chegaraddi@intel.com>

--- drivers/bluetooth/btusb.c
+++ drivers/bluetooth/btusb.c
@@ -3835,16 +3835,15 @@ static int btusb_probe(struct usb_interface *intf,
 	if (id->driver_info & BTUSB_AMP) {
 		/* AMP controllers do not support SCO packets */
 		data->isoc = NULL;
+#if (!IS_ENABLED(CONFIG_BT_SCOHCI))
 	} else {
 		/* Interface orders are hardcoded in the specification */
 		data->isoc = usb_ifnum_to_if(data->udev, ifnum_base + 1);
 		data->isoc_ifnum = ifnum_base + 1;
 		hdev->wbs_pkt_len =
 			hci_packet_size_usb_alt[btusb_find_altsetting(data, 6) ?
-							6 :
-						btusb_find_altsetting(data, 3) ?
-							3 :
-							      1];
+			6 : btusb_find_altsetting(data, 3) ? 3 :1];
+#endif
 	}
 
 	if (IS_ENABLED(CONFIG_BT_HCIBTUSB_RTL) &&
